8a367dba814e999a1eb4c0ad03579f5c6a311ccb
jimmysorza <28790876+JimmySorza@users.noreply.github.com>
2020-05-15 11:16:45 -0500

    add app id

--- envs.js
+++ envs.js
@@ -1,4 +1,4 @@
-export const TEST_APP_ID = BUILD_APP_ID || undefined;
+export const TEST_APP_ID = '06ED278F-61BD-4BE3-84A1-A093FEC346DB';
 export const USER_ID = undefined;
 export const ACCESS_TOKEN = undefined;
 export const IS_ACCESS_TOKEN_NEEDED = false;
